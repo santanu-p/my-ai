4a6ef3c7a9386b4824961067d4735b2997af574e
Santanu Pradhan <psantanu293@gmail.com>
2025-07-02 13:30:56 +0530

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Github, MessagesSquare } from "lucide-react"
+import { Globe, MessagesSquare } from "lucide-react"
 import Link from "next/link"
 import {
   Sidebar,
@@ -20,12 +20,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-                <Link href="https://assistant-ui.com" target="_blank">
+                <Link href="#" target="_blank" rel="noopener noreferrer">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                     <MessagesSquare className="size-4" />
                   </div>
                   <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">assistant-ui</span>
+                    <span className="font-semibold">Zin Drab Theta</span>
                   </div>
                 </Link>
               </SidebarMenuButton>
@@ -42,13 +42,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
          
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
+              <Link href="https://santanu-p.github.io/personal-webpage/" target="_blank" rel="noopener noreferrer">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Github className="size-4" />
+                  <Globe className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">GitHub</span>
-                  <span className="">View Source</span>
+                  <span className="font-semibold">Website</span>
+                  <span className="">Visit Site</span>
                 </div>
               </Link>
             </SidebarMenuButton>
